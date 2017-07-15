2c688a6d63090c68676935a16c358158f4029353
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-07-15 22:40:20 +1000

    Another Fi

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://<lowfi>:<password123>@ds151941.mlab.com:51941/rest-api'
+    mlab: 'mongodb://<lowfi>:<password123456>@ds151941.mlab.com:51941/rest-api'
 };
 
 // - (protocol:mongodb://URL/DatabaseName)
